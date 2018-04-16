6a62a131434147d75c79874aaef3e8f2321da3aa
Crixorum1992 <nicjreid@ut.utm.edu>
2018-04-16 12:18:53 -0500

    Updated Code

--- Warhammer Database/Database2/MainWindow.xaml.cs
+++ Warhammer Database/Database2/MainWindow.xaml.cs
@@ -28,32 +28,19 @@ namespace Database2
 
         OleDbConnection cn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\Nicholas Reid\\source\\repos\\Database2\\Database2\\Warhammer.mdb");
         string data = "";
+		database datab = new database();
         private void Infantry_Click(object sender, RoutedEventArgs e)
         {
             string query = "SELECT * FROM Infantry";
-            int x = 3;
-            int y = 0;
             OleDbCommand cmd = new OleDbCommand(query, cn);
             cn.Open();
             OleDbDataReader reader = cmd.ExecuteReader();
 
             //string test = "3501";
-
-            if (x == 0)
-            {
-                while (reader.Read())
-                {
-                    data += reader[0].ToString() + "/n";
-                }
-            }
-            
-            else if (x == 7)
-            {
-                while (reader.Read())
-                {
-                    data += reader[0].ToString() + reader[1].ToString() + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString() + " " + reader[7].ToString() + " " + "\n";
-                }
-            }
+			while (reader.Read())
+			{
+              data += reader[0].ToString() + reader[1].ToString() + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString() + " " + reader[7].ToString() + " " + "\n";
+             }
 
             TextBox.Text = data;
             cn.Close();
@@ -61,45 +48,56 @@ namespace Database2
 
         private void Vehicle_Click(object sender, RoutedEventArgs e)
         {
-            //string query = "SELECT UnitName FROM Vehicles";
-            /*OleDbCommand cmd = new OleDbCommand(query, cn);
-            cn.Open();
-            OleDbDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                data += reader[0].ToString() + "\n";
-            }
-            TextBox.Text = data;
-            cn.Close();*/
-        }
+            string query = "SELECT * FROM Vehicles";
+			OleDbCommand cmd = new OleDbCommand(query, cn);
+			cn.Open();
+			OleDbDataReader reader = cmd.ExecuteReader();
+
+			//string test = "3501";
+			while (reader.Read())
+			{
+				data += reader[0].ToString() + reader[1].ToString() + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString() + " " + "\n";
+			}
+
+			TextBox.Text = data;
+			cn.Close();
+		}
 
         private void Walker_Click(object sender, RoutedEventArgs e)
         {
-            //string query = "SELECT UnitName FROM Walkers";
-            /*OleDbCommand cmd = new OleDbCommand(query, cn);
-            cn.Open();
-            OleDbDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                data += reader[0].ToString() + "\n";
-            }
-            TextBox.Text = data;
-            cn.Close();*/
-        }
+			string query = "SELECT * FROM Walkers";
+			OleDbCommand cmd = new OleDbCommand(query, cn);
+			cn.Open();
+			OleDbDataReader reader = cmd.ExecuteReader();
+
+			//string test = "3501";
+			while (reader.Read())
+			{
+				data += reader[0].ToString() + reader[1].ToString() + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString() + " " + "\n";
+			}
+
+			TextBox.Text = data;
+			cn.Close();
+		}
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //string query = "SELECT * FROM ID";
-            /*OleDbCommand cmd = new OleDbCommand(query, cn);
-            cn.Open();
-            OleDbDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                data += reader[0].ToString() + "\n";
-            }
-            TextBox.Text = data;
-            cn.Close();*/
-        }
+            string query = "SELECT * FROM Infantry";
+			/*OleDbCommand cmd = new OleDbCommand(query, cn);
+			cn.Open();
+			OleDbDataReader reader = cmd.ExecuteReader();
+
+			//string test = "3501";
+			while (reader.Read())
+			{
+				data += reader[0].ToString() + reader[1].ToString() + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString() + " " + reader[7].ToString() + " " + "\n";
+			}
+
+			TextBox.Text = data;
+			cn.Close();*/
+			data = datab.accDatabase(query);
+			TextBox.Text += data;
+		}
         /*private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox.Text = data;
@@ -118,12 +116,12 @@ namespace Database2
             OleDbDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                data += reader[0].ToString() + "\n";
-            }
-            //TextBox.Text = data;
-            cn.Close();
+				data += reader[0].ToString() + reader[1].ToString() + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString() + " " + reader[6].ToString() + " " + reader[7].ToString() + " " + "\n";
+			}
+			//TextBox.Text = data;
+			cn.Close();
 
-            return "01";
+            return data;
         }
     }
 }
